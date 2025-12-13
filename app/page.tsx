780db21fc0720900087a759fa7c582d2d7345f18
sandeep <sedar.sandeep@gmail.com>
2025-12-13 16:02:57 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 
 export default function Home() {
+
+  console.log("THIS IS JENKIN PROJECT LOG1212");
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
      This Is Jenkin Project first-1
