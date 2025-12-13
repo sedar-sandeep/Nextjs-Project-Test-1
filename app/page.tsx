d99ae830399a26c2ac28154678db6fc131238d5b
sandeep <sedar.sandeep@gmail.com>
2025-12-13 18:53:28 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   console.log("THIS IS JENKIN PROJECT LOG1212");
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     This Is Jenkin Project first-1232331***********
+     This Is Jenkin Project first-55555***************######
     </div>
   );
 }
